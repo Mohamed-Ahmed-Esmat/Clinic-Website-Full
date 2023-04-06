82d132cb5730df409f46bf802b8bb4bf58d67128
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-04-06 14:49:07 +0200

    Main changes in creating databse for Availabledate

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id = int
-    name = str
-    description = str
+class Feature(models.Model):
+     name = models.CharField(max_length=100, default='')
+     details = models.CharField(max_length=500, default='')
+
+class Available_Resv(models.Model):
+     day = models.IntegerField(default=31)
+     month = models.CharField(max_length=10, default='December')
+     year = models.IntegerField(default=2023)
+     time = models.CharField(max_length=10, default='12:00')
+
+class Available_Tel(models.Model):
+     day = models.IntegerField(default=31)
+     month = models.CharField(max_length=10, default='December')
+     year = models.IntegerField(default=2023)
+     time = models.CharField(max_length=10, default='12:00')
+
+class Available_Oper(models.Model):
+     day = models.IntegerField(default=31)
+     month = models.CharField(max_length=10, default='December')
+     year = models.IntegerField(default=2023)
+     time = models.CharField(max_length=10, default='12:00')
+
+class Booking(models.Model):
+     type = models.CharField(max_length=20, default='')
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Feature
+from .models import Available_Resv
+from .models import Available_Tel
+from .models import Available_Oper
+from .models import Booking
 
 # Create your views here.
 def index(request):
-    features = Feature()
-    features.id = 1
-    features.name = 'Feature 1'
-    features.description = 'This is the first feature'
-    
-    return render(request, 'index.html', {'feature': features})
+    features = Feature.objects.all()
+    available_resv = Available_Resv.objects.all()
+    available_tel = Available_Tel.objects.all()
+    available_oper = Available_Oper.objects.all()
+    booking = Booking.objects.all()
+    return render(request, 'index.html', {'features': features, 'available_resv': available_resv, 'available_tel': available_tel, 'available_oper': available_oper, 'booking': booking})
+
+
+
 
 #def counter(request):
  #   text = request.POST['text']
@@ -17,7 +24,11 @@ def index(request):
 #   return render(request, 'counter.html', {'count': words_count})
 
 def contact(request):
-    return render(request, 'contact.html')
+    available_resv = Available_Resv.objects.all()
+    available_tel = Available_Tel.objects.all()
+    available_oper = Available_Oper.objects.all()
+    booking = Booking.objects.all()
+    return render(request, 'contact.html', {'available_resv': available_resv, 'available_tel': available_tel, 'available_oper': available_oper, 'booking': booking })
 def news(request):
     return render(request, 'news.html')
 def health(request):
