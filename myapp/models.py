1598a2b5c35afc9b505d7e87deb6b2a3774e4c76
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-05-03 12:49:55 +0300

    f

--- myapp/models.py
+++ myapp/models.py
@@ -11,5 +11,12 @@ class Available_Date(models.Model):
      year = models.IntegerField(default=2023)
      time = models.CharField(max_length=10, default='12:00')
 
+     def __str__(self):
+          return f"{self.month} {self.day}"
+
 class Booking(models.Model):
      type = models.CharField(max_length=20, default='')
+
+     def __str__(self):
+          return self.type
+          
