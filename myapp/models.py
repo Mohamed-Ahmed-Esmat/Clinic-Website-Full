4301ec8e79fbb8b3b31299bd6ca3fea321bfa9a8
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-05-16 00:35:23 +0300

    some changes

--- myapp/models.py
+++ myapp/models.py
@@ -5,8 +5,6 @@ class Available_Date(models.Model):
      day = models.IntegerField(default=31)
      month = models.CharField(max_length=3, default='Dec')
      year = models.IntegerField(default=2023)
-     time = models.CharField(max_length=10, default='12:00')
-     isAvailable = models.BooleanField(default=True)
 
      def __str__(self):
           return f"{self.month} {self.day}"
