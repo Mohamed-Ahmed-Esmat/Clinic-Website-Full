d27fbef9a588232cc2fc4e1982f6d2a662dff786
AbdoAhmed24 <128639117+AbdoAhmed24@users.noreply.github.com>
2023-05-15 22:48:56 +0300

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -6,7 +6,6 @@ from .models import Patient
 
 # Register your models here.
 
-admin.site.register(Feature)
 admin.site.register(Available_Date)
 admin.site.register(Booking)
 admin.site.register(Patient)
