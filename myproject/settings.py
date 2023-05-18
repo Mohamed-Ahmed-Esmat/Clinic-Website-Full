2e0c786d81aad0647b74f69bc95240527d626afa
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-05-18 14:26:46 +0300

    deployement trial on vercel

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b#s7d^&huticd(ek9y9+vx9au4@n^13urjvk&*ass*un@_7msb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
